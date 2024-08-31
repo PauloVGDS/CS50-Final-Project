62fd61d16cf1747f03184de4f8940cc5d1f839d2
PauloVGDS <paulovinicius1732@gmail.com>
2024-08-30 21:44:11 -0300

    cleanup: substituição por inline css.

--- extension/popup.js
+++ extension/popup.js
@@ -17,11 +17,24 @@ async function infoAPI() {
     })
       .then(httpInfo => httpInfo.json())
   }
+
+  
   catch(error) {
     // Se der erro imprime na tela
     console.log(`Erro: ${error}`);
   }
   finally {
+
+    if (code == "") {
+      template = `
+      <a href="#" id="packageEventPlaceholder" class="list-group-item list-group-item-action" aria-current="true">
+        <div class="d-flex w-100 justify-content-between align-items-center">
+          <p class="mb-1 lead fw-semibold">Your package informations will appears here!</p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" height="80px" width="80px"><path d="m720-80 120-120-28-28-72 72v-164h-40v164l-72-72-28 28L720-80ZM480-800 243-663l237 137 237-137-237-137ZM120-321v-318q0-22 10.5-40t29.5-29l280-161q10-5 19.5-8t20.5-3q11 0 21 3t19 8l280 161q19 11 29.5 29t10.5 40v159h-80v-116L479-434 200-596v274l240 139v92L160-252q-19-11-29.5-29T120-321ZM720 0q-83 0-141.5-58.5T520-200q0-83 58.5-141.5T720-400q83 0 141.5 58.5T920-200q0 83-58.5 141.5T720 0ZM480-491Z"/></svg>
+        </div>
+      </a>`
+      return container.innerHTML = template;
+    }
     // Ordenação dos elementos da array por ordem de chegada
     template = ""
     data.eventos.sort((a, b) => {
@@ -30,13 +43,12 @@ async function infoAPI() {
     return a + b
     });
 
-
     // Loop para colocar as informações de cada evento dentro da div 
     for (let i = 0; i < data["eventos"].length; i++) {
       template += `
       <a href="#" id="packageEvent" class="list-group-item list-group-item-action" aria-current="true">
         <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">${data.eventos[i].status}</h5>
+          <h5 id="eventHeader" class="mb-1">${data.eventos[i].status}</h5>
         </div>
         <p class="mb-1">${data.eventos[i].subStatus[0]}</p>
         <small>${data.eventos[i].subStatus[1] != undefined ? data.eventos[i].subStatus[1] : ""}</small>
